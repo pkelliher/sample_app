f72ad266821a15bc0111ee29e0b51f40e32c40bb
pkelliher <pkelliher74@gmail.com>
2020-11-15 17:59:37 -0800

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,9 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
+  
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
